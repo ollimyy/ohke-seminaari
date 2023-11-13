c103816bc48075dac0556e04486cefd7ebf2cdbf
Topias <you@example.com>
2023-11-13 15:54:50 +0200

    Add database access and query by keyword

---
+++ get_data_from_db.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+import re
+import pandas as pd
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_URL = os.getenv("DB_URL")
+DB_NAME = os.getenv("DB_NAME")
+ITEM_COLLECTION = os.getenv("ITEM_COLLECTION")
+
+# MongoDB connection
+client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_URL}")
+db = client[DB_NAME]
+item_collection = db[ITEM_COLLECTION]
+
+def find_by_keyword(query):
+    return list(item_collection.find(query))
+
+def get_data_frame(item_description):
+    query = {
+        'title': {
+            '$regex': re.compile(item_description, re.IGNORECASE),
+            } 
+        }
+
+    item_data_df = pd.DataFrame(find_by_keyword(query))
+
+    return item_data_df
