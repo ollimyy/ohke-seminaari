97f5a91471d3b433d1fc362e0fb5254507a6deff
Olli Myyryläinen <98151462+ollimyy@users.noreply.github.com>
2023-11-15 10:22:19 +0200

    Change dotenv to python-dotenv in requirements. Remove redundant variables and imports. Update comments.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Query
 from price_estimator import estimate_price_from_condition
-import re
 
 app = FastAPI()
 
@@ -9,9 +8,6 @@ async def estimate_price(
     item_description: str = Query(..., description="Description of the item"),
     condition: int = Query(..., description="Condition of the item (1-5)", ge=1, le=5)
 ):
-
-    item_description = item_description
-    condition = condition
 
     price_estimate = estimate_price_from_condition(condition, item_description)
 
--- price_estimator.py
+++ price_estimator.py
@@ -1,14 +1,13 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-from sample_data_generator import generate_sample_data
 from get_data_from_db import get_data_frame
 
 def estimate_price_from_condition(condition: int, item_description: str):
     if condition < 1 or condition > 5:
         raise ValueError('Condition must be between 1 and 5')
 
-    # TODO: replace with getting the real data, but for now, generate sample data
+    # get data matching the item description
     data = get_data_frame(item_description)
 
     # separate condition and price columns
