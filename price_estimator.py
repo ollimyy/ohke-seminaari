3b5d3d6febb19816bf574917237542102423f7c4
Olli Myyryläinen <98151462+ollimyy@users.noreply.github.com>
2023-12-01 22:24:06 +0200

    Add plotting function to price_estimator.

--- price_estimator.py
+++ price_estimator.py
@@ -1,6 +1,7 @@
+import sys
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
 from get_data_from_db import get_data_frame
 
 def estimate_price_from_condition(condition: int, item_description: str):
@@ -23,3 +24,47 @@ def estimate_price_from_condition(condition: int, item_description: str):
     result = model.predict([[condition]])
 
     return int(result[0])
+
+def estimate_and_plot(condition: int, item_description: str):
+
+    # Estimate price
+    estimated_price = estimate_price_from_condition(condition, item_description)
+
+    print(f"Estimated price for '{item_description}', condition {condition}: {estimated_price}")
+
+    # Plot the linear regression line
+    data = get_data_frame(item_description)
+    X = data[['condition']]
+    y = data['price']
+
+    model = LinearRegression()
+    model.fit(X.values, y)
+
+    # Plot the data points
+    plt.scatter(X, y, color='blue', label='Data Points')
+
+    # Plot the linear regression line
+    x_range = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
+    y_range = model.predict(x_range)
+    plt.plot(x_range, y_range, color='red', linewidth=3, label='Linear Regression')
+    
+    # Plot the estimation
+    plt.scatter(condition, estimated_price, color='green', marker='o', s=100, zorder=10, label='Estimated Price')
+
+    plt.xticks([1, 2, 3, 4, 5])
+    plt.xlabel('Condition')
+    plt.ylabel('Price')
+    plt.title(f'"{item_description}", condition: {condition}, estimated price: {estimated_price}')
+    plt.legend()
+    plt.grid(True)
+
+    # Save the plot to a file
+    plt.savefig('estimation_plot.png')
+
+# figures can be plotted from the commandline
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python plot_estimation.py <item_description> <condition>")
+        sys.exit(1)
+
+    estimate_and_plot(int(sys.argv[2]), sys.argv[1])
